4e36453cf7fdfa3ef1dc09c2f5d69927404108cb
rlaxogud0909 <rlaxogud0909@gmail.com>
2022-02-19 10:14:55 +0900

    Material UI2

--- React 기초/Ch 03. 리액트 라이브러리/ui/src/Components/MaterialUI/MaterialUIExample.jsx
+++ React 기초/Ch 03. 리액트 라이브러리/ui/src/Components/MaterialUI/MaterialUIExample.jsx
@@ -3,13 +3,26 @@
 // FAQ -> 실무과정에서 마주할 이슈들 정리
 // Layout / Theme -> 체계화 된 시스템
 
+//api 구성 -> Components / api / Demo
+// makeStyles -> 컴포넌트의 구조를 관장
+// Virtual List -> Virtual scroll
+// System -> 순수한 커스텀 컴포넌트 생성 가능
+
 import React from "react";
 import ButtonExample from "./ButtonExample";
 import CenteredGrid from "./CenteredGrid";
+import CheckboxLabels from "./CheckboxLabels";
+import IconLabelButtons from "./IconLabelButtons";
+import TemporaryDrawer from "./TemporaryDrawer";
+import VirtualizedList from "./VirtualizedList";
 
 export default function MaterialUIExample() {
     return (
         <div>
+            <VirtualizedList />
+            <TemporaryDrawer />
+            <CheckboxLabels />
+            <IconLabelButtons />
             <CenteredGrid />
             <ButtonExample />
         </div>
