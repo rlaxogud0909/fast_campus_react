4bf7be6d82f333d85a2fab07374c4da9b7188144
rlaxogud0909 <rlaxogud0909@gmail.com>
2022-02-21 16:03:44 +0900

    react spring

---
+++ React 기초/Ch 03. 리액트 라이브러리/animate/src/App.js
@@ -0,0 +1,13 @@
+import logo from "./logo.svg";
+import "./App.css";
+import ReactSpringExample from "./components/ReactSpringExample/Example";
+
+function App() {
+    return (
+        <div className="App">
+            <ReactSpringExample />
+        </div>
+    );
+}
+
+export default App;
---
+++ React 기초/Ch 03. 리액트 라이브러리/animate/src/components/ReactSpringExample/Example.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import Number from "./Number";
+import Text from "./Text";
+
+export default function Example() {
+    return (
+        <div>
+            <Number />
+            <Text />
+        </div>
+    );
+}
