49c5a34481357cd268fbd518bc297e024feef0e4
rlaxogud0909 <rlaxogud0909@gmail.com>
2022-02-22 15:06:27 +0900

    ReactSemanticUI

--- React 기초/Ch 03. 리액트 라이브러리/ui/src/App.js
+++ React 기초/Ch 03. 리액트 라이브러리/ui/src/App.js
@@ -1,14 +1,28 @@
 import "./App.css";
 // Webpack CSS import
-import "onsenui/css/onsenui.css";
-import "onsenui/css/onsen-css-components.css";
+// import "onsenui/css/onsenui.css";
+// import "onsenui/css/onsen-css-components.css";
 // import OnsenUIExample from "./Components/OnsenUI/OnsenUIExample";
-import AntDesignExample from "./Components/AntDesign/AntDesignExample";
+
+// import AntDesignExample from "./Components/AntDesign/AntDesignExample";
+
+// import "semantic-ui-css/semantic.min.css";
+// import ReactSemanticUIExample from "./Components/ReactSemanticUI/ReactSemanticUIExample";
+
+// import "bootstrap/dist/css/bootstrap.min.css";
+// import ReactBootstrapExample from "./Components/ReactBootstrap/ReactBootstrapExample";
+
+// import MaterialUIExample from "./Components/MaterialUI/MaterialUIExample";
+import Example from "./Components/Tailwind/Example";
 
 function App() {
     return (
         // <div className="App">
-        <AntDesignExample />
+        <Example />
+        // <MaterialUIExample />
+        // <ReactBootstrapExample />
+        // <ReactSemanticUIExample />
+        // <AntDesignExample />
         // <OnsenUIExample />
         // </div>
     );
--- React 기초/Ch 03. 리액트 라이브러리/ui/src/Components/MaterialUI/MaterialUIExample.jsx
+++ React 기초/Ch 03. 리액트 라이브러리/ui/src/Components/MaterialUI/MaterialUIExample.jsx
@@ -3,7 +3,7 @@
 // FAQ -> 실무과정에서 마주할 이슈들 정리
 // Layout / Theme -> 체계화 된 시스템
 
-//api 구성 -> Components / api / Demo
+// api 구성 -> Components / api / Demo
 // makeStyles -> 컴포넌트의 구조를 관장
 // Virtual List -> Virtual scroll
 // System -> 순수한 커스텀 컴포넌트 생성 가능
