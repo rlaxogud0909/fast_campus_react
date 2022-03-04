9895605560cea4575f1240f103802486a8d0511e
rlaxogud0909 <rlaxogud0909@gmail.com>
2022-03-04 18:07:16 +0900

    SWR 2

--- React 기초/Ch 06. 데이터 페칭 라이브러리/fetch/src/App.js
+++ React 기초/Ch 06. 데이터 페칭 라이브러리/fetch/src/App.js
@@ -1,6 +1,8 @@
 import "./App.css";
 import Chache from "./components/SWRExample/Chache";
 import Fetcher from "./components/SWRExample/Fetcher";
+import Mutate from "./components/SWRExample/Mutate";
+import Pagination from "./components/SWRExample/Pagination";
 // import TestMocking from "./components/TestMocking";
 import Profile from "./components/SWRExample/Profile";
 
@@ -10,7 +12,9 @@ function App() {
             {/* <TestMocking /> */}
             {/* <Profile /> */}
             {/* <Chache /> */}
-            <Fetcher />
+            {/* <Fetcher /> */}
+            <Mutate />
+            <Pagination />
         </div>
     );
 }
