777f4d93ab8bd175205abc4072459806b237585c
rlaxogud0909 <rlaxogud0909@gmail.com>
2022-03-16 23:23:55 +0900

    react-game: 가위바위보

---
+++ React game/5. 가위바위보/client.jsx
@@ -0,0 +1,7 @@
+const React = require("react");
+const ReactDom = require("react-dom");
+
+import RSP from "./RSP";
+// const RSP = require("./RSP");
+
+ReactDom.render(<RSP />, document.querySelector("#root"));
